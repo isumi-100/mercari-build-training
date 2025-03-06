34abfd1b205a50f70bd6cbb28cfd71d08bc26374
isumi-100 <kk.100.koizu@gmail.com>
2025-03-07 00:41:09 +0900

    sql修正

--- go/db/items.sql
+++ go/db/items.sql
@@ -1,6 +1,12 @@
 CREATE TABLE items (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name TEXT NOT NULL,
-category TEXT NOT NULL,
-image_name TEXT NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    category_id INTEGER NOT NULL,
+    image_name TEXT NOT NULL,
+    UNIQUE(name, image_name)
+);
+
+CREATE TABLE categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
 );
